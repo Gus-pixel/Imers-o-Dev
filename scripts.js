1fca5cf2d807ae975df298f3e1d324bf2494d24c
Gustavo Ramos <gustavo.ramos7049@gmail.com>
2023-01-31 14:05:05 -0300

    Criando estilo e fazendo a primeira função

--- scripts.js
+++ scripts.js
@@ -1,6 +1,15 @@
-var valueInDollar = 64;
-var dollarPrice = 5.32;
+function exchange() {
+  var name = document.getElementById("name").value;
+  var valueInDollar = document.getElementById("value").value;
+  var dollarPrice = 5.1;
 
-var valueInReal = valueInDollar * dollarPrice;
-valueInReal = valueInReal.toFixed(2);
-alert(`R$ ${valueInReal}`);
+  var valueInReal = valueInDollar * dollarPrice;
+  valueInReal = valueInReal.toFixed(2);
+  valueInDollar = Number.parseFloat(valueInDollar);
+  valueInDollar = valueInDollar.toFixed(2);
+  if (!isNaN(valueInDollar)) {
+    alert(`Olá ${name}, $${valueInDollar} são equivalentes a R$${valueInReal}`);
+  } else {
+    alert("Por favor, preencha o campo com o valor desejado..");
+  }
+}
