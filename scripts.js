2c72c0359e4af9cb730380647793874cf03e6d79
Gustavo Ramos <gustavo.ramos7049@gmail.com>
2023-01-31 22:31:09 -0300

    Fazendo request na api

--- scripts.js
+++ scripts.js
@@ -1,17 +1,29 @@
-function exchange() {
-  var name = document.getElementById("name").value;
-  var valueInDollar = document.getElementById("value").value;
-  var dollarPrice = 5.1;
+var dollarPrice;
 
-  var valueInReal = valueInDollar * dollarPrice;
-  valueInReal = valueInReal.toFixed(2);
-  valueInDollar = Number.parseFloat(valueInDollar);
-  valueInDollar = valueInDollar.toFixed(2);
-  if (!isNaN(valueInDollar)) {
-    alert(`Olá ${name}, $${valueInDollar} são equivalentes a R$${valueInReal}`);
-  } else {
-    alert("Por favor, preencha o campo com o valor desejado..");
-  }
+function exchange() {
+  fetch("http://economia.awesomeapi.com.br/json/last/USD")
+    .then((result) => result.json())
+    .then((data) => {
+      dollarPrice = data.USDBRL.ask;
+      dollarPrice = parseFloat(dollarPrice);
+      dollarPrice = dollarPrice.toFixed(2);
+
+      var name = document.getElementById("name").value;
+      var valueInDollar = document.getElementById("value").value;
+
+      var valueInReal = valueInDollar * dollarPrice;
+      valueInReal = valueInReal.toFixed(2);
+      valueInDollar = Number.parseFloat(valueInDollar);
+      valueInDollar = valueInDollar.toFixed(2);
+      if (!isNaN(valueInDollar)) {
+        alert(
+          `Olá ${name}, $${valueInDollar} são equivalentes a R$${valueInReal}`
+        );
+      } else {
+        alert("Por favor, preencha o campo com o valor desejado..");
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
 function clearFields() {
